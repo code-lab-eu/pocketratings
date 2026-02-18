1a1f55b90eea1c31a174014ea98f27232aeaeebc
Pieter Frenssen <pieter@frenssen.be>
2026-02-19 00:09:35 +0200

    Remove the placeholder for the frontend.

--- frontend/nuxt.config.ts
+++
@@ -1,5 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-02-15',
-  modules: ['@nuxt/eslint', '@nuxt/test-utils'],
-})
--- frontend/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>Pocket Ratings</h1>
-  </div>
-</template>
